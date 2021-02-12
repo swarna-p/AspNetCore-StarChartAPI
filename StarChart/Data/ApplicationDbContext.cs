55ec5cf0ffa4107b38487b12f3380ce662febc75
swarna-p <swarnaj@gmail.com>
2021-02-12 13:30:55 +0000

    Add files via upload

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects;
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+            
         }
     }
 }
