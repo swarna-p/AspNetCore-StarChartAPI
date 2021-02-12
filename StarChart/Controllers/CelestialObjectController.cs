1736fb3af48fe7aa8a25fb2893aca154252efd3d
swarna-p <swarnaj@gmail.com>
2021-02-12 14:06:27 +0000

    Add files via upload

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,8 @@
+﻿using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
 using StarChart.Data;
 
 
@@ -15,5 +19,36 @@ namespace StarChart.Controllers
             _context = context;
        }
 
+       [HttpGet("GetById/{id:int}")]
+       public IActionResult GetById(int id)
+       {
+           var celestialObject = _context.CelestialObjects.FirstOrDefault(o => o.Id == id);
+           if(celestialObject !=null)
+           {
+                return Ok(celestialObject);
+           }
+           return NotFound($"The id: {id} cannot be found in the database");
+       }
+
+       [HttpGet("{name}")]
+       public IActionResult GetByName(string name)
+       {
+           var celestialObject = _context.CelestialObjects.FirstOrDefault(o => o.Name == name);
+           if (celestialObject != null)
+           {
+               return Ok(celestialObject);
+           }
+           return NotFound($"The object {name} cannot be found in the database");
+       }
+       [HttpGet()]
+       public IActionResult GetAll()
+       {
+           var celestialObjects = _context.CelestialObjects.ToList();
+           foreach(var obj  in celestialObjects)
+           {
+                obj.Satellites = celestialObjects;
+           }
+            return Ok(celestialObjects);
+       }
     }
 }
